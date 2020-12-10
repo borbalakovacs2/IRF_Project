18aae2a7931b73c59f5e85e595bb2a7269ff8968
borbalakovacs2 <borbala.kovacs2@stud.uni-corvinus.hu>
2020-12-10 07:42:51 +0100

    commit

--- Bevasarlo/Bevasarlo/Form1.Designer.cs
+++ Bevasarlo/Bevasarlo/Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxTermek = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMennyi = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.cbVegan = new System.Windows.Forms.CheckBox();
+            this.cbGluten = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBoxTermekek = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxTermek
+            // 
+            this.textBoxTermek.Location = new System.Drawing.Point(12, 41);
+            this.textBoxTermek.Name = "textBoxTermek";
+            this.textBoxTermek.Size = new System.Drawing.Size(190, 22);
+            this.textBoxTermek.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Termék";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mennyiség";
+            // 
+            // textBoxMennyi
+            // 
+            this.textBoxMennyi.Location = new System.Drawing.Point(12, 103);
+            this.textBoxMennyi.Name = "textBoxMennyi";
+            this.textBoxMennyi.Size = new System.Drawing.Size(56, 22);
+            this.textBoxMennyi.TabIndex = 2;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(74, 102);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(23, 23);
+            this.btnPlus.TabIndex = 4;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(103, 102);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(23, 23);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // cbVegan
+            // 
+            this.cbVegan.AutoSize = true;
+            this.cbVegan.Location = new System.Drawing.Point(12, 145);
+            this.cbVegan.Name = "cbVegan";
+            this.cbVegan.Size = new System.Drawing.Size(71, 21);
+            this.cbVegan.TabIndex = 7;
+            this.cbVegan.Text = "Vegán";
+            this.cbVegan.UseVisualStyleBackColor = true;
+            // 
+            // cbGluten
+            // 
+            this.cbGluten.AutoSize = true;
+            this.cbGluten.Location = new System.Drawing.Point(12, 172);
+            this.cbGluten.Name = "cbGluten";
+            this.cbGluten.Size = new System.Drawing.Size(118, 21);
+            this.cbGluten.TabIndex = 8;
+            this.cbGluten.Text = "Gluténmentes";
+            this.cbGluten.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Egyéb jellemző";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 42);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Listához ad";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBoxTermekek
+            // 
+            this.listBoxTermekek.FormattingEnabled = true;
+            this.listBoxTermekek.Location = new System.Drawing.Point(400, 41);
+            this.listBoxTermekek.Name = "listBoxTermekek";
+            this.listBoxTermekek.Size = new System.Drawing.Size(237, 310);
+            this.listBoxTermekek.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxTermekek);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cbGluten);
+            this.Controls.Add(this.cbVegan);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxMennyi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTermek);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxTermek;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMennyi;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.CheckBox cbVegan;
+        private System.Windows.Forms.CheckBox cbGluten;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox listBoxTermekek;
     }
 }
 
---
+++ Bevasarlo/Bevasarlo/Termek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bevasarlo
+{
+    
+    class Termek
+    {
+        public string Nev { get; set; }
+        public int Mennyiseg { get; set; }
+        public string Mertekegyseg { get; set; }
+        public bool Vegan { get; set; }
+        public bool Glutenmentes { get; set; }
+        public string Egyeb { get; set; }
+    }
+
+}
